80910b35b1007b443140aee354a73d5224d419af
rami3l <rami3l@outlook.com>
2021-09-12 08:28:11 +0000

    Deploying to gh-pages from @ rami3l/pacaptr@b1b19d5ffe79deb65c4362d782bf4569bba1fe74 🚀

--- pacaptr_macros/sidebar-items.js
+++ pacaptr_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"macro":[["compat_table",""],["test_dsl","A DSL (Domain-Specific Language) embedded in Rust, in order to simplify the form of smoke tests."]]});
+initSidebarItems({"macro":[["compat_table","Generates the compatibility table as a docstring on the top of given input."],["test_dsl","A DSL (Domain-Specific Language) embedded in Rust, in order to simplify the form of smoke tests."]]});
