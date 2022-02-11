6a0b35099e6daa404ebb8ab57281d46fb0230ee7
rami3l <rami3l@outlook.com>
2022-02-11 23:21:48 +0000

    Deploying to gh-pages from @ rami3l/pacaptr@28753b2f8a0f3dec812fc42c2560c3dd7cfed3ba 🚀

--- tabled/style/sidebar-items.js
+++ tabled/style/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Line","Line represents a horizontal line on a [Table]."],["Style","Style is responsible for a look of a [Table]."]]});
+initSidebarItems({"struct":[["CustomStyle","CustomStyle represents a style controlling a valid state of it."],["On",""],["Style","Style is represents a theme of a [Table]."],["StyleSettings",""],["TopBorderText","Style is responsible for a look of a [Table]."]]});
